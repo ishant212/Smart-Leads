ef1c256a726872dc66755bc69944e73ef4fd23a5
Ishant Shekhar Eeshu <ishantvats123@gmail.com>
2026-05-20 08:20:50 +0530

    Updated production backend URL

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  //baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-leads-api-ttq2.onrender.com/api",
 });
 
 export default API;
